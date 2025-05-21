28aab51dc4f8e7f870e942914ec04c29fe1ce040
XusniddinQalandarov <x.qalandarov@newuzbekistanuniversity.uz>
2025-05-21 16:07:52 +0500

    added two topics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,37 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { SetterGetterComponent } from './setter-getter/setter-getter.component';
+import { Component, inject } from '@angular/core';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { TranslocoService, TranslocoModule } from '@ngneat/transloco';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    RouterLink,
+    RouterLinkActive,
+    TranslocoModule,
+    CommonModule,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'blogs';
+  private translocoService = inject(TranslocoService);
+  availableLangs: string[] | readonly string[];
+  activeLang: string;
+  isSidebarOpen = false;
+
+  constructor() {
+    this.availableLangs = this.translocoService.getAvailableLangs() as string[];
+    this.activeLang = this.translocoService.getActiveLang();
+  }
+
+  changeLang(lang: string) {
+    this.translocoService.setActiveLang(lang);
+    this.activeLang = lang;
+  }
+
+  toggleSidebar() {
+    this.isSidebarOpen = !this.isSidebarOpen;
+  }
 }
