5e47252798a1a055e9a7c7897a53a67f2ee1440f
XusniddinQalandarov <x.qalandarov@newuzbekistanuniversity.uz>
2025-05-21 11:55:27 +0500

    blog initialized

---
+++ src/app/setter-getter/setter-getter.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-setter-getter',
+  imports: [],
+  templateUrl: './setter-getter.component.html',
+  styleUrl: './setter-getter.component.scss'
+})
+export class SetterGetterComponent {
+
+}
